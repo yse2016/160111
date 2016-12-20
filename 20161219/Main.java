ac24ebdace0980a1f23ca45a1297dd8e4235b01f
fhayahide <fukuda_hayahide@yse-c.net>
2016-12-20 13:10:10 +0900

    added

--- 20161219/Main.java
+++ 20161219/Main.java
@@ -3,13 +3,20 @@ import javax.swing.*;
 public class Main {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame(/*"はじめてのGUI"*/);
-		JLabel label = new JLabel("Aloha!!");
-		//JButton button = new JButton("押してね");
+		//JLabel label = new JLabel("Aloha!!");
+		for(int i=0;i<70;i++){
+			if(i%3 != 0){
+		JButton button = new JButton("押してね");
+		frame.getContentPane().add(button);
+			}else{
+		JButton button = new JButton("押し○す");
+		frame.getContentPane().add(button);				
+			}
+		}
 		frame.getContentPane().setLayout(new FlowLayout());
-		frame.getContentPane().add(label);
-		//frame.getContentPane().add(button);
+		//frame.getContentPane().add(label);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(300,100);
+		frame.setSize(500,500);
 		frame.setVisible(true);
 	}
 }
