59bd8d1b576dcc094380c3ac4b03067818c6ea8d
fhayahide <fukuda_hayahide@yse-c.net>
2017-01-10 16:06:28 +0900

    add

--- 20170110/Aloha.java
+++ 20170110/Aloha.java
@@ -1,5 +1,23 @@
 public class Aloha{
 	public static void main(String[] args) {
-		System.out.println("Aloha!");
+		Alohaman am = new Alohaman();
+		am.sayAloha(11);
+	}
+}
+
+
+class Alohaman{
+	private String msg1;
+	private String msg2;
+
+	public Alohaman(){
+		this.msg1 = "Aloha!";
+		this.msg2 = "アロハ！";
+	}
+
+	public void sayAloha(int n){
+		for (int i=0;i<n;i++) {
+		System.out.println(i+":"+this.msg1);
+		}
 	}
 }
