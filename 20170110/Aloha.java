1399d1334177e451cc7e498e80bef2cb67b9ca22
fhayahide <fukuda_hayahide@yse-c.net>
2017-01-17 16:04:53 +0900

    add

--- 20170110/Aloha.java
+++ 20170110/Aloha.java
@@ -1,7 +1,20 @@
 public class Aloha{
 	public static void main(String[] args) {
 		Alohaman am = new Alohaman();
-		am.sayAloha(11);
+		int num = args.length;
+		if(num==0){
+		am.sayAloha();
+		}else if(num==1){
+		int a = Integer.parseInt(args[0]);
+		am.sayAloha(a);
+		}else if(num==2){
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		am.sayAloha(a,b);
+		}else{
+			System.out.println("入力したデータが正しくありません");
+			System.out.println("データの数は0～2個でお願いします");
+		}
 	}
 }
 
@@ -15,9 +28,24 @@ class Alohaman{
 		this.msg2 = "アロハ！";
 	}
 
+	public void sayAloha(){
+		System.out.println(this.msg1);
+	}
+
 	public void sayAloha(int n){
 		for (int i=0;i<n;i++) {
-		System.out.println(i+":"+this.msg1);
+			System.out.println(i+":"+this.msg1);
+		}
+	}
+
+	public void sayAloha(int n,int x){
+		for (int i=0;i<n;i++) {
+			if(i%x==0){
+				System.out.println(i+":"+this.msg1);
+			}else{
+				System.out.println(i+":"+this.msg2);
+
+			}
 		}
 	}
 }
